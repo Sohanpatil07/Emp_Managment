cd9e8fa7d6007baac7946d89c1513f1d93ce6880
Sohan <159467173+Sohanpatil07@users.noreply.github.com>
2025-01-18 18:35:38 +0530

    repository layer updated

--- src/main/java/com/crud/repository/Emp_Repository.java
+++ src/main/java/com/crud/repository/Emp_Repository.java
@@ -3,6 +3,6 @@ package com.crud.repository;
 import com.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Emp_Repositoy extends JpaRepository<Long, Employee> {
+public interface Emp_Repository extends JpaRepository<Long, Employee> {
 
 }
